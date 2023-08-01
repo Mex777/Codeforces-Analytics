de23b060f25311e4c378d7357bb4c14324beaa8b
Mex777 <fologeaalexandru@gmail.com>
2023-08-01 22:14:57 +0300

    Backend refactoring
    Deleted unnecessary code from app.py

--- backend/app/app.py
+++ backend/app/app.py
@@ -3,12 +3,6 @@ from backend.app.config import DATABASE_CONNECTION_URL
 from backend.app.extensions import db
 from backend.app.models.models import User, Problem, Tag, Contest
 from backend.app.models.tables import problem_tags, user_solved_problems, user_contests
-import sys
-import os
-
-# Add the parent directory (project directory) to the Python path
-project_path = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(project_path, ".."))
 
 # creating the Flask app
 app = Flask(__name__)
@@ -24,7 +18,3 @@ with app.app_context():
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-
-
-if __name__ == '__main__':
-    app.run()
