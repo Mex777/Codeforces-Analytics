ca9b56ab6a5f61b45ccc64d553814ea77e2eab98
Mex777 <fologeaalexandru@gmail.com>
2023-08-01 22:12:52 +0300

    Backend update.
    Added the database models in python and exported them to the database.

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
-from config import DATABASE_CONNECTION_URL
-from flask_sqlalchemy import SQLAlchemy
+from backend.app.config import DATABASE_CONNECTION_URL
+from backend.app.extensions import db
+from backend.app.models.models import User, Problem, Tag, Contest
+from backend.app.models.tables import problem_tags, user_solved_problems, user_contests
+import sys
+import os
 
-# creating the database instance
-db = SQLAlchemy()
+# Add the parent directory (project directory) to the Python path
+project_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(project_path, ".."))
 
 # creating the Flask app
 app = Flask(__name__)
@@ -12,9 +17,14 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_CONNECTION_URL
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 
+if __name__ == '__main__':
+    app.run()
